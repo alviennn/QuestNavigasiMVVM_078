c8e890fc97b9bd6293c75a30d09d2c4424bf336b
alviennn <alvien.rnp@gmail.com>
2024-11-21 12:56:20 +0700

    Menambahkan enum class 'Halaman'

---
+++ app/src/main/java/com/example/navigation/PengelolaHalaman.kt
@@ -0,0 +1,23 @@
+package com.example.navigation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigation.model.DataJK
+import com.example.navigation.ui.view.FormMahasiswaView
+import com.example.navigation.ui.viewmodel.MahasiswaViewModel
+
+enum class Halaman{
+
+}
